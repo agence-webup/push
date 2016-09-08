10a03a49ba535f666f7c60124cac33313b4dceaf
Matthieu Martin <matthieu@agence-webup.com>
2016-09-08 11:54:28 +0200

    Add the language field to Token

--- tokens.go
+++ tokens.go
@@ -2,10 +2,13 @@ package push
 
 import "fmt"
 
+// Platform is an enum representing supported Push platforms
 type Platform int
 
 const (
-	IOS     Platform = 1
+	// IOS represents APNS platform
+	IOS Platform = 1
+	// Android represents GCM platform
 	Android Platform = 2
 )
 
@@ -14,6 +17,7 @@ type Token struct {
 	UUID     string   `json:"uuid"`
 	Value    string   `json:"token"`
 	Platform Platform `json:"platform"`
+	Language string   `json:"language"`
 }
 
 // Validate returns an error if the Token is not valid
@@ -27,9 +31,13 @@ func (t *Token) Validate() error {
 	if t.Platform != IOS && t.Platform != Android {
 		return fmt.Errorf("'platform' is required and must be 1 (iOS) or 2 (Android)")
 	}
+	if t.Language == "" {
+		return fmt.Errorf("'language' is required")
+	}
 	return nil
 }
 
+// TokenRepository defines the behavior for interacting with tokens
 type TokenRepository interface {
 	FindTokenWithValue(value string) (Token, error)
 	RemoveTokenWithValue(value string) error
